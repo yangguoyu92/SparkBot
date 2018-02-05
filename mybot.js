fdba7d2c475e487e71c4012523e545b7ebae0eb5
EC2 Default User <ec2-user@ip-172-31-12-190.ap-southeast-1.compute.internal>
2018-02-05 15:03:36 +0000

    second version

--- mybot.js
+++ mybot.js
@@ -6,7 +6,6 @@ var app = express();
 var fs = require("fs");
 var redisDriver = require('node-flint/storage/redis');
 
-
 app.use(bodyParser.json());
 
 // flint options
@@ -26,47 +25,52 @@ flint.start();
 //set redis as storage
 flint.storageDriver(redisDriver('redis://localhost'));
 
-var state = 'query';
-var inputAcro;
+//define help instructions
+flint.hears('/help', function(bot, trigger) {
+  bot.say('Hello %s!\n' +
+  'If you want to search for an acronym, just send it to me!\n' +
+  'If you want to add a new item to the library, please enter in this format: /add ACI Application Centric Infrastructure', 
+  trigger.personDisplayName);
+});
+
+//add an item
+flint.hears('/add', function(bot, trigger) {
+  bot.say('Thanks for your contribution!');
+  var text = trigger.text;
+  var array = text.split(' ');
+  console.log(array);
+  var key = array[1];
+  array.splice(0, 2);
+  var value = array.join(' ');
+  console.log('key: ' + key);
+  console.log('value: '+ value);
+  if(key == undefined || value == '') {
+    bot.say('Please enter in this format: /add ACI Application Centric Infrastructure');
+  }
+  bot.store(key, value);
+});
+
+flint.hears('/hello', function(bot, trigger) {
+    bot.say('Hello %s! Type /help to see more instructions.', trigger.personDisplayName);
+});
+
 // add flint event listeners
 flint.on('message', function(bot, trigger, id) {
   //flint.debug('"%s" said "%s" in room "%s"', trigger.personEmail, trigger.text, trigger.roomTitle);
   console.log('"%s" said "%s" in room "%s"', trigger.personEmail, trigger.text, trigger.roomTitle);
-  if(trigger.personEmail != 'acro@sparkbot.io') {
+  var prefix = trigger.text.split(' ')[0];
+  if(trigger.personEmail != 'acro@sparkbot.io' && prefix != '/add' && prefix != '/help' && prefix != '/hello') {
     var text = trigger.text;
-    
-    switch (state) {
-      case 'query':
-        var value = bot.recall(text);
-        console.log(value);
-        if(value == undefined) {
-          state = 'ask';
-          inputAcro = text;
-          bot.say('No result. Would you like to add this to the library? (yes/no)');
-        }else {
-          bot.say(value);
-        }
-        break;
-      case 'ask':
-        if(text == 'yes') {
-          state = 'add';
-          bot.say('Please send me the explaination.');
-        }else {
-          state = 'query';
-        }
-        break;
-      case 'add':
-        bot.store(inputAcro, text);
-        state = 'query';
-        bot.say('Thanks for your contribution!');
-        break;
-      default:
-        // code
+    var value = bot.recall(text);
+    if(value == undefined) {
+      bot.say('No result. If you want to add this new item to the library, please enter in the format: /add ACI Application Centric Infrastructure');
+    }else {
+      bot.say(value);
     }
   }
 });
 
-flint.on('initialized', function() {
+flint.on('initialized', function(bot) {
   //flint.debug('initialized %s rooms', flint.bots.length);
   console.log('initialized %s rooms', flint.bots.length);
 });
